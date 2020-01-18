e8e80227639520e990694d33cea86b81310ea149
Deadzie <46291667+Deadzie@users.noreply.github.com>
2020-01-18 11:25:27 -0500

    done?

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 from flaskwebgui import FlaskUI   # get the FlaskUI class
 
 
@@ -8,16 +9,13 @@ ui = FlaskUI(app)                 # feed the parameters
 
 # do your logic as usual in Flask
 
-@app.route("/")
-def index():
-    return '''
+#@app.route("/")
+#def index():
+#    return '''     '''
 
-<html>
-    <head>
-        <title>{{ title }} - Microblog</title>
-    </head>
-    <body>
-        <h1>Hello, {{ user.username }}!</h1>
-    </body>
-</html>'''
+@app.route('/')
+def index():
+    your_list= [1,2,3,4]
+    return render_template('your_view.html', your_list=your_list)
+
 ui.run()
--- list.py
+++ list.py
@@ -1,41 +1,48 @@
-import sys
 import datetime
 
 
-f= open("list_doc","r")
-sys.stdout.write
+f= open("list_doc","r+")
 list = f.read().splitlines()
 
+def list_disp() :
+    print(15* '\n')
+    for task in list:
+        if task == "press 'a' to add an item" or task == "press 'x' to remove an item" or task == "type 'end' to exit":
+                print(task)
+        else :
+                print(list.index(task) + 1, task)
+    print(1 * '\n')
 
-for item in list:
-    if item == "press 'a' to add an item" or item == "press 'x' to remove an item":
-        print(item)
-    else :
-      print(list.index(item) + 1,item)
-
-
+list_disp()
 u_in = input('')
+while u_in != 'end' :
+
+    if u_in == 'a' :
+        print('please input the item')
+        item = input('')
+        list.insert(0, str(item))
+        list_disp()
+        u_in = input('')
 
 
-if u_in == 'a' :
-    print('please input the item')
-    item = input('')
-    list.insert(0, str(item))
+    if u_in == 'x':
+        print('input the number before the item you want to delete')
+        u_in2 = int(input())
+        f2 = open("success_doc", "a+")
+        f2.write( 'You completed ' + list[u_in2-1] + '!' + ' '*(70 - (len('You completed ' + list[u_in2-1]))) + str(datetime.datetime.now()) + '\n')
 
-if u_in == 'x':
-    print('input the number before the item you want to delete')
-    u_in2 = int(input())
-    f2 = open("success_doc", "w+")
-    f2.write( 'You completed ' + list[u_in2-1] + '!                                  ' + str(datetime.datetime.now()))
+        f2 = open("success_doc", "r")
+        list.pop(u_in2 - 1)
+        count = len(f2.readlines())
 
-    list.pop(u_in2 - 1)
-    total_succ = f2.read().splitlines()
-    print(total_succ)
+        list_disp()
+        print('Congratulations! You have completed ' + str(count) + ' total tasks!')
+        u_in = input('')
 
 
-print('...')
-f= open("list_doc","w")
+    print('...')
 
-for item in list:
-    f.write("%s\n" % item)
+
+    for item in list:
+        f.write("%s\n" % item)
 f.close()
