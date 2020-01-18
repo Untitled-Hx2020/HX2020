877611fd2fd73eea081d59cc694820fbe4b6ba18
Deadzie <46291667+Deadzie@users.noreply.github.com>
2020-01-17 23:04:10 -0500

    progress

--- list.py
+++ list.py
@@ -1,11 +1,14 @@
 import sys
+import datetime
+
+
 f= open("list_doc","r")
 sys.stdout.write
 list = f.read().splitlines()
 
 
 for item in list:
-    if item == "press 'a' to add an item":
+    if item == "press 'a' to add an item" or item == "press 'x' to remove an item":
         print(item)
     else :
       print(list.index(item) + 1,item)
@@ -21,8 +24,13 @@ if u_in == 'a' :
 
 if u_in == 'x':
     print('input the number before the item you want to delete')
-    u_in2 = input()
+    u_in2 = int(input())
+    f2 = open("success_doc", "w+")
+    f2.write( 'You completed ' + list[u_in2-1] + '!                                  ' + str(datetime.datetime.now()))
 
+    list.pop(u_in2 - 1)
+    total_succ = f2.read().splitlines()
+    print(total_succ)
 
 
 print('...')
