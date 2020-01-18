771ae764253156e112913126ddd242deb2d0c6e9
Deadzie <46291667+Deadzie@users.noreply.github.com>
2020-01-17 20:41:23 -0500

    added stuff

--- list.py
+++ list.py
@@ -1,16 +1,33 @@
 import sys
-f= open("list_doc","w+")
+f= open("list_doc","r")
 sys.stdout.write
-list = []
-list2 = ['Add item to list? (press a)']
-list.extend(list2)
-print(list)
+list = f.read().splitlines()
+
+
+for item in list:
+    if item == "press 'a' to add an item":
+        print(item)
+    else :
+      print(list.index(item) + 1,item)
+
+
 u_in = input('')
+
+
 if u_in == 'a' :
     print('please input the item')
     item = input('')
-    list.insert(0, item)
+    list.insert(0, str(item))
+
+if u_in == 'x':
+    print('input the number before the item you want to delete')
+    u_in2 = input()
+
+
 
 print('...')
-sys.stdout.write(str(list))
+f= open("list_doc","w")
+
+for item in list:
+    f.write("%s\n" % item)
 f.close()
